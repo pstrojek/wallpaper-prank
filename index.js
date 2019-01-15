e6d97b61b27dae191462523dead081cb6de0b279
Pawel Strojek <paulstrojek@gmail.com>
2019-01-15 22:11:42 +0100

    Add first version of wallpaper-prank

---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const wallpaper = require('wallpaper')
+const fs = require('fs');
+const path = require('path');
+const parseArgs = require('minimist')
+const isUrl = require('is-url-superb');
+const got = require('got');
+const tempfile = require('tempfile');
+
+const params = parseArgs(process.argv.slice(2));
+const INTERVAL_TIME = 60000
+const wallpaperList = ['wallpaper1.jpg', 'wallpaper2.jpg']
+
+let state = {
+  imageUrl: '',
+  choosenWalpaper: 0,
+  wallpaperName: '',
+}
+
+state.imageUrl = params.url || ''
+state.wallpaperName = wallpaperList[state.choosenWalpaper]
+
+const setWallpaper = async (name, imageUrl) => {
+  const currentWallpaper = await wallpaper.get()
+  if (isUrl(imageUrl) && !currentWallpaper.endsWith(name)) {
+    const file = tempfile(path.extname(imageUrl))
+    state.wallpaperName = path.basename(imageUrl)
+  
+    got
+      .stream(imageUrl)
+      .pipe(fs.createWriteStream(file))
+      .on('finish', async () => {
+        await wallpaper.set(file);
+      })
+  } else if (!currentWallpaper.endsWith(name)) {
+      await wallpaper.set(__dirname + '/' + name)
+  }
+}
+
+setWallpaper(state.wallpaperName, state.imageUrl)
+if (params.watch) {
+  setInterval(() => {
+    setWallpaper(state.wallpaperName, state.imageUrl)
+  }, INTERVAL_TIME)
+}
